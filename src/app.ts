98b5be013563a4e472bebd27cf5da669154e8510
1B0is <boingboing831@gmail.com>
2026-03-05 17:30:03 +0700

    .

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,29 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./db";
 import authRoutes from "./routes/authRoutes";
 import pageRoutes from "./routes/pageRoutes";
+import path from "path";
+
 const app = express();
-// important behind Render proxy
+
 app.set("trust proxy", 1);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static("src/public"));
+
+app.use(express.static(path.join(__dirname, "../src/public")));
 app.set("view engine", "ejs");
-app.set("views", "./src/views");
+app.set("views", path.join(__dirname, "../src/views"));
+
 app.use(authRoutes);
 app.use(pageRoutes);
-const PORT = Number(process.env.PORT || 4000 );
+
+const PORT = Number(process.env.PORT || 4000);
+
 async function main() {
-await connectDB(process.env.MONGODB_URI!);
-app.listen(PORT, () => console.log(`✅ http://localhost:${PORT}`));
+  await connectDB(process.env.MONGODB_URI!);
+  app.listen(PORT, "0.0.0.0", () => console.log(`✅ http://localhost:${PORT}`));
 }
+
 main().catch((err) => {
-console.error("❌ Startup error:", err);
-process.exit(1);
+  console.error("❌ Startup error:", err);
+  process.exit(1);
 });
